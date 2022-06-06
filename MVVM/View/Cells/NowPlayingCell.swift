4f60cc63d5a181e55b161bc40c952f234a2c793f
Gulam Ali <gulam2275@gmail.com>
2022-06-06 18:45:48 +0530

    delete movie

--- MVVM/View/Cells/NowPlayingCell.swift
+++ MVVM/View/Cells/NowPlayingCell.swift
@@ -28,6 +28,7 @@ class NowPlayingCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- MVVM/View/Controller/NowPlayingVC.swift
+++ MVVM/View/Controller/NowPlayingVC.swift
@@ -51,12 +51,30 @@ class NowPlayingVC: UIViewController {
         CommonFuncs.AlertWithOK(msg: "\(votes)", vc: self)
     }
     
+    
+    //MARK: Diffable snapshots
+    
+    private func CreateSnapshot(from list:[resultsModel]){
+        var snap = SnapShot()
+        snap.appendSections([.first])
+        snap.appendItems(list)
+        datasourcee.apply(snap, animatingDifferences: true, completion: nil)
+    }
+    
+    private func deleteMovie(from list:resultsModel,index:IndexPath){
+        guard let objectIClickedOnto = datasourcee.itemIdentifier(for: index) else { return }
+        var snapshot = datasourcee.snapshot()
+        snapshot.deleteItems([objectIClickedOnto])
+        datasourcee.apply(snapshot, animatingDifferences: true)
+        nowPlayingViewModel.Movies.remove(at: index.row)
+    }
+    
 
 }
 
 // MARK: -  Tableview protocols
 extension NowPlayingVC : UITableViewDelegate{
-        
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 180.0
     }
@@ -66,18 +84,25 @@ extension NowPlayingVC : UITableViewDelegate{
         print("Selected movie is -> ",hasMovieData.title ?? "")
     }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { [self] (action, sourceView, completionHandler) in
+            print("index path of delete: \(indexPath)")
+            deleteMovie(from: nowPlayingViewModel.Movies[indexPath.row], index: indexPath)
+            completionHandler(true)
+        }
+        
+        let swipeActionConfig = UISwipeActionsConfiguration(actions: [delete])
+        swipeActionConfig.performsFirstActionWithFullSwipe = false
+        return swipeActionConfig
+    }
+    
 }
 
 // MARK: -  NowPlayingVM protocols
 extension NowPlayingVC : NowPlayingProtocol{
     
     
-    private func CreateSnapshot(from list:[resultsModel]){
-        var snap = SnapShot()
-        snap.appendSections([.first])
-        snap.appendItems(list)
-        datasourcee.apply(snap, animatingDifferences: true, completion: nil)
-    }
+
     
     func didgetMovies(Movies: [resultsModel]) {
         if Movies.count != 0{
